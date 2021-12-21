eb85c75b594ee9f4788b088322c2c8d5a1d05270
TheNuelgeek <thenuelgeekmain@gmail.com>
2021-12-21 18:44:37 +0100

    Intial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function rot13(str_text) {
+    
+    const str = str_text;
+    const getMap = (legend, shift) => {
+       return legend.reduce((charsMap, currentChar, charIndex) => {
+          const copy = { ...charsMap };
+          let ind = (charIndex + shift) % legend.length;
+          if (ind < 0) {
+             ind += legend.length;
+          };
+          copy[currentChar] = legend[ind];
+          return copy;
+       }, {});
+    };
+
+    const encrypt = (str, shift = 0) => {
+       const legend = 'abcdefghijklmnopqrstuvwxyz'.split('');
+       const map = getMap(legend, shift);
+       return str
+       .toLowerCase()
+       .split('')
+       .map(char => map[char] || char)
+       .join('');
+    };
+    return encrypt(str, 6);
+};
